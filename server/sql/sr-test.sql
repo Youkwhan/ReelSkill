09080d282f9b64bab9359083c099cee8a01b7e83
Youkwhan <Youkwhan@gmail.com>
2024-05-01 15:07:56 -0400

    feat(DML): populate the database and set known good state

--- server/sql/sr-test.sql
+++ server/sql/sr-test.sql
@@ -64,3 +64,74 @@ CREATE TABLE recentlyDeleted (
     deleted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+delimiter //
+
+create procedure set_known_good_state()
+begin
+
+    delete from users;
+    alter table users auto_increment = 1;
+    delete from decks;
+    alter table decks auto_increment = 1;
+    delete from cardType;
+    alter table cardType auto_increment = 1;
+    delete from cardTag;
+    alter table cardTag auto_increment = 1;
+    delete from cards;
+    alter table cards auto_increment = 1;
+    delete from recentlyDeleted;
+    alter table recentlyDeleted auto_increment = 1;
+
+    -- data
+    INSERT INTO users (email_address, username, `password`, `role`)
+    VALUES
+        ('user1@example.com', 'user1', 'password1', 'user'),
+        ('user2@example.com', 'user2', 'password2', 'user'),
+        ('admin@example.com', 'admin', 'adminpassword', 'admin');
+
+    INSERT INTO decks (user_id, deck_name)
+    VALUES
+        (1, 'Two Pointer'),
+        (1, 'Binary Search'),
+        (2, 'Depth-First Search');
+
+
+    INSERT INTO cardType (cardType_name) VALUES
+        ('easy'),
+        ('medium'),
+        ('hard');
+
+    INSERT INTO cardTag (cardTag_name) VALUES
+        ('easy'),
+        ('medium'),
+        ('hard');
+
+    INSERT INTO cards (deck_id, card_title, card_notes, leetcode_problem, cardType_id, cardTag_id)
+    VALUES
+        (1, '1. Two Sum', 'With using the Two Pointers pattern, and Pointer 1 pointing to the beginning of the array and Pointer 2 pointing to the end of the array, we will check if the numbers pointed by the pointers add up to the target sum. If they do, we have found our pair. If not, we should do one of these things:
+
+                          • If the sum is bigger than the target sum, this means that we need a smaller sum so, we are going to decrement the Pointer 2 (end-pointer).
+
+                          • If the sum is smaller than the target sum, this means that we need a bigger sum so, we are going to increment the Pointer 1 (start-pointer).',
+          'https://leetcode.com/problems/two-sum/description/',
+          1, null),
+        (1, '15. 3Sum', 'This problem is a follow-up of the Two Sum problem, but instead of finding two numbers that add up to a target sum, you need to find three numbers that add up to the target sum. The approach is to fix one number and then find the other two numbers using the Two Pointers pattern.',
+          'https://leetcode.com/problems/3sum/description/',
+          2, null),
+        (2, '704. Binary Search', 'Binary Search is a classic algorithm to efficiently find an element in a sorted array. The key idea is to repeatedly divide the search interval in half until the target element is found or the interval is empty.',
+          'https://leetcode.com/problems/binary-search/description/',
+          1, null),
+        (2, '56. Merge Intervals', 'Given a collection of intervals, merge all overlapping intervals. The key idea is to sort the intervals based on their start times and then merge intervals by checking for overlaps.',
+          'https://leetcode.com/problems/merge-intervals/description/',
+          2, null),
+        (3, '94. Binary Tree Inorder Traversal', 'Depth-First Search (DFS) is a fundamental algorithm for traversing or searching tree or graph data structures. The algorithm starts at the root node and explores as far as possible along each branch before backtracking.',
+          'https://leetcode.com/problems/binary-tree-inorder-traversal/description/',
+          1, null);
+
+    INSERT INTO recentlyDeleted (card_id, card_title, card_notes, leetcode_problem, number_of_times_reviewed, last_reviewed)
+    VALUES
+        (3, '124. Binary Tree Maximum Path Sum', 'For every node checking what could be the possible max path sum considering the subtrees and passing that max length to the parent. Now but the answer could be max of this path sum or left subtree max path sum till now + right subtree max path sum till now + node value or only the node value', 'https://leetcode.com/problems/binary-tree-maximum-path-sum/description/', 3, '2024-04-30 12:00:00');
+
+end //
+
+delimiter;
