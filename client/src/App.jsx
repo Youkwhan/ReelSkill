f77b57f95985cca0f9413817812ff1a01586cc08
Youkwhan <Youkwhan@gmail.com>
2024-05-03 13:01:51 -0400

    folder and file structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import { Home, Login } from './scenes';
+import Navbar from './components/Navbar';
+
 function App() {
-  return <></>;
+  return (
+    <Router>
+      <Navbar />
+      <main className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </main>
+    </Router>
+  );
 }
 
 export default App;
